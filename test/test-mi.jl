9bbee769545a9c5d69c9530d6df385b2f5724690
LislPisl <eroesch@student.unimelb.edu.au>
2019-04-01 21:04:33 +1100

    export  #Please enter the commit message for your changes. Lines starting

--- test/test-mi.jl
+++ test/test-mi.jl
@@ -2,33 +2,28 @@
 push!(LOAD_PATH,"/Users/eroesch/github/DiffEqMI.jl/src/")
 include("../src/DiffEqMI.jl")
 using OrdinaryDiffEq, Optim
-
-
-###########################  by user ##########################
+########################### in by user ##########################
 true_init = [2.; 0.]
 true_params = [ -2.0 ,-0.1,-0.1, 2.0]
 total_tspan = (0., 3.)
 datasize = 30
 n_subs = 3
 n_species = 2
-
+#this we could maybe hide better
 total_t =  range(total_tspan[1], total_tspan[2], length = datasize)
 true_sys = DiffEqMI.diffeqflux_example(true_params)
 prob = ODEProblem(true_sys, true_init, total_tspan)
 ode_data = Array(solve(prob,Tsit5(),saveat=total_t))
 noisy_data = DiffEqMI.add_noise(ode_data, 0.6)
-##############################################################
+##################### visualize true ########################
 using Plots
 plot(total_t, ode_data[1,:], label="ode data")
 scatter!(total_t, noisy_data[1,:], label="ode data")
-##############################################################
+######################## start positions #######################
 u0_start_values = [[2.; 0.], [1.; 0.], [1.; 0.]]
 params_start_values = [ -2.0 ,-0.04,-0.5, 2.0]
 x= vcat(u0_start_values...,params_start_values)
-##############################################################
-
-
-
+####################### init multi init  #########################
 test =DiffEqMI.tester()
 DiffEqMI.init_tester(
                 test,
