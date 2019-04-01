08fdc6cb455d0d333059f35459002ff351e21cb7
LislPisl <eroesch@student.unimelb.edu.au>
2019-04-01 21:26:58 +1100

    delete push loadpath

--- test/test-mi.jl
+++ test/test-mi.jl
@@ -1,5 +1,3 @@
-
-push!(LOAD_PATH,"/Users/eroesch/github/DiffEqMI.jl/src/")
 include("../src/DiffEqMI.jl")
 using OrdinaryDiffEq, Optim
 ########################### in by user ##########################
@@ -14,11 +12,11 @@ total_t =  range(total_tspan[1], total_tspan[2], length = datasize)
 true_sys = DiffEqMI.diffeqflux_example(true_params)
 prob = ODEProblem(true_sys, true_init, total_tspan)
 ode_data = Array(solve(prob,Tsit5(),saveat=total_t))
-noisy_data = DiffEqMI.add_noise(ode_data, 0.6)
+noisy_data = DiffEqMI.add_noise(ode_data, 0.2)
 ##################### visualize true #############################
 using Plots
 plot(total_t, ode_data[1,:], label="ode data")
-scatter!(total_t, noisy_data[1,:], label="ode data")
+scatter!(total_t, noisy_data[1,:], label="noisy data")
 ######################## start positions #########################
 u0_start_values = [[2.; 0.], [1.; 0.], [1.; 0.]]
 params_start_values = [ -2.0 ,-0.04,-0.5, 2.0]
@@ -43,10 +41,10 @@ result_one = Optim.optimize(L2_loss_fct, x )
 using Plots
 species = 1
 plot(total_t, ode_data[species,:], label="ode data")
-scatter!(total_t, noisy_data[species,:], label="ode data")
+scatter!(total_t, noisy_data[species,:], label="noisy data")
 start_sys = DiffEqMI.diffeqflux_example(params_start_values)
 start_prob = ODEProblem(start_sys, u0_start_values[1], total_tspan)
 start_ode_data = Array(solve(start_prob,Tsit5(),saveat=total_t))
 plot!(total_t, start_ode_data[1,:], label="start optim")
-plot!(total_t, test.sols[species,:], label="end sol")
+plot!(total_t, test.sols[species,:], label="end optim")
 ###################################################################
